ceaf3b674d263733fb91e04bcac3934cd999f41c
jflaney23 <jordanflana@gmail.com>
2022-10-08 12:13:08 -0400

    Initial Setup

---
+++ colorMatching.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+import numpy as np
+
+img = cv2.imread('/Users/jflaney23/Developer/OpenCV Project/assets/Screen Shot 2022-10-08 at 12.02.13 PM.png')
+
+#Convers to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+#ranges of blue in HSV
+#Lightest HSV hsv(192, 11%, 89%)
+#Darkest HSV hsv(192, 43%, 83%)
+
+
+mask = cv2.inRange(hsv, (85, 40, 40), (100,255,255))
+ 
+#slices the blue
+imask = mask>0
+blue = np.zeros_like(img, np.uint8)
+blue[imask] = img[imask]
+
+#saves the image
+cv2.imwrite("blue2.png", blue)
